8c34b38e18b774b902cd92750a5bbbb5a92ffdc0
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2022-12-10 19:50:17 +0530

    Create main.py

---
+++ ShoppingCart/main.py
@@ -0,0 +1,30 @@
+products = [
+
+    {
+
+        "name": "apple",
+
+        "price": 0.99
+
+    },
+
+    {
+
+        "name": "banana",
+
+        "price": 0.59
+
+    },
+
+    {
+
+        "name": "orange",
+
+        "price": 0.69
+
+    },
+
+    # ...
+
+]
+
